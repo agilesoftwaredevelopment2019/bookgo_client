7e56a77ecf97b5dab30db9e231a276f581269cc2
Robert Kim <jrheonghwan@naver.com>
2019-04-05 12:28:44 +0900

    first init

---
+++ app/src/main/java/com/example/robertkim/bookgo/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.robertkim.bookgo.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.robertkim.bookgo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
